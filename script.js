f4d1b6655ebe7b04092a5fd9ff513419c11e865a
paubel <paul.belfrage@gmail.com>
2023-11-15 22:35:31 +0100

    fix resp

--- script.js
+++ script.js
@@ -156,6 +156,16 @@ var temps = [
 ];
 var colorScale = d3.scaleLinear().domain(temps).range(colors);
 
+// Add headline
+svg
+  .append("text")
+  .attr("class", "headline")
+  .attr("x", width / 2)
+  .attr("y", margin.top / 2 + 10)
+  .attr("text-anchor", "middle")
+  .style("font-size", "24px")
+  .style("text-decoration", "none")
+  .text("Hertzsprung-Russell Diagram");
 // Create circles for stars on the HR diagram with radii based on the star size and colored by temperature
 svg
   .selectAll("circle")
@@ -169,10 +179,11 @@ svg
 
 // Add labels to stars
 svg
-  .selectAll("text")
+  .selectAll(".star-label") // Add a class here
   .data(stars)
   .enter()
   .append("text")
+  .attr("class", "star-label") // And here
   .attr("x", (d) => xScale(d.temperature) + 10)
   .attr("y", (d) => yScale(d.luminosity))
   .text((d) => d.name)
@@ -181,6 +192,7 @@ svg
   .style("text-shadow", "2px 2px 4px #000"); // Add a black text shadow
 // Add x-axis
 const xAxis = d3.axisBottom(xScale).ticks(5, "s");
+
 svg
   .append("g")
   .attr("class", "x-axis")
@@ -207,49 +219,12 @@ svg
 // Add y-axis label
 svg
   .append("text")
+  .attr("class", "y-label")
   .attr("x", -height / 2)
   .attr("y", 10)
   .attr("transform", "rotate(-90)")
   .style("text-anchor", "middle")
   .text("Luminosity (Solar Units)");
-
-window.addEventListener("resize", () => {
-  width = parseInt(container.style("width"));
-  height = parseInt(container.style("height"));
-
-  // Update the position of the x-axis label
-  svg
-    .select(".x-label")
-    .attr("x", width / 2)
-    .attr("y", height - margin.bottom + 20);
-
-  // Update the viewBox of the SVG
-  svg.attr("viewBox", `0 0 ${width} ${height}`);
-
-  // Update the scales
-  xScale.range([margin.left, width - margin.right]);
-  yScale.range([height - margin.bottom, margin.top]);
-
-  // Redraw the axes
-  svg
-    .select(".x-axis")
-    .attr("transform", `translate(0, ${height - margin.bottom})`)
-    .call(d3.axisBottom(xScale));
-
-  svg.select(".y-axis").call(d3.axisLeft(yScale));
-
-  // Update the positions of the circles
-  svg
-    .selectAll("circle")
-    .attr("cx", (d) => xScale(d.temperature))
-    .attr("cy", (d) => yScale(d.luminosity));
-
-  // Update the positions of the text labels
-  svg
-    .selectAll("text")
-    .attr("x", (d) => xScale(d.temperature))
-    .attr("y", (d) => yScale(d.luminosity));
-});
 
 svg
   .selectAll("circle")
@@ -274,3 +249,51 @@ svg
   .on("mouseout", () => {
     tooltip.style("display", "none");
   });
+
+window.addEventListener("resize", () => {
+  width = parseInt(container.style("width"));
+  height = parseInt(container.style("height"));
+
+  // Update the position of the x-axis label
+  svg
+    .select(".x-label")
+    .attr("x", width / 2)
+    .attr("y", height - margin.bottom + 10);
+
+  // Update the viewBox of the SVG
+  svg.attr("viewBox", `0 0 ${width} ${height}`);
+
+  // Update the scales
+  xScale.range([margin.left, width - margin.right]);
+  yScale.range([height - margin.bottom, margin.top]);
+
+  // Redraw the axes
+  svg
+    .select(".x-axis")
+    .attr("transform", `translate(0, ${height - margin.bottom})`)
+    .call(d3.axisBottom(xScale));
+
+  svg.select(".y-axis").call(d3.axisLeft(yScale));
+
+  // Update the positions of the circles
+  svg
+    .selectAll("circle")
+    .attr("cx", (d) => xScale(d.temperature))
+    .attr("cy", (d) => yScale(d.luminosity));
+
+  // Update the position of the headline
+  svg.select(".headline").attr("x", width / 2);
+
+  svg.select(".y-label").attr("x", -height / 2);
+
+  // Update the position of the x-axis label
+  svg
+    .select(".x-label")
+    .attr("x", width / 2)
+    .attr("y", height - margin.bottom + 30); // Adjust this value as needed
+
+  svg
+    .selectAll(".star-label") // Select only the star labels
+    .attr("x", (d) => xScale(d.temperature) + 10)
+    .attr("y", (d) => yScale(d.luminosity));
+});
